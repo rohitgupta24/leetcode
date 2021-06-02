30eb6ef7abe75457e6dcd9f8d7e1f4949ddc23b6
rohitgupta24 <rohit@allps.ch>
2021-06-02 17:00:18 +0530

    binary_search

---
+++ binary_search.py
@@ -0,0 +1,30 @@
+# works only for sorted list
+
+def binary_search(arr, target):
+    left = 0
+    right =len(arr)-1
+
+    while(left <= right):
+        mid = (left + right)//2   #0 5 ==> (0+5)//2 = 2
+
+        if(arr[mid]) == target:
+            return mid
+
+        elif(arr[mid] < target):
+            left = mid+1
+
+        else:
+            right = mid -1
+
+    return -1
+
+arr = [1,2,3,4,5,6]
+target = 6
+
+result = binary_search(arr, target) 
+
+if result != -1:
+    print("Element is present at index %d" % result)
+
+else:
+    print("Element is not present in array")
